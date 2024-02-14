cf7cc698f001aad7267ee2226d0c925528c20e98
jinylol <160023723+jinylol@users.noreply.github.com>
2024-02-14 14:31:56 -0800

    create project

---
+++ OddEvanGame/OddEvanGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OddEvanGame
+//
+//  Created by Geena Kim on 2024/02/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
